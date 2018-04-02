f7acd2e34c267f70fe059987643d5f4ad68c0c51
klamm <klammrock@gmail.com>
2018-04-02 19:00:45 +0400

    alpha API

---
+++ blog_service/blog_service.go
@@ -0,0 +1,32 @@
+package blog_service
+
+import (
+	"fmt"
+)
+
+// [url]/api/v1/posts
+// GET posts/id
+// { id, html, }
+//
+// POST posts/id
+// PUT posts/id
+// DELETE posts/id
+
+// comments
+// GET for post, POST add to post
+
+// categories
+
+////
+// html
+// [url]/ ?page=
+// [url]/about
+// [url]/posts/[post_id]
+// [url]/categories/[category_id]
+// [url]/admin
+// [url]/admin/posts
+// [url]/admin/logs
+
+func Do_work() {
+	fmt.Println("hello blog")
+}
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"simple_blog/blog_service"
+	//"github.com/weekface/mgorus"
+	//"gopkg.in/mgo.v2"
 )
 
 func main() {
 	fmt.Println("hello")
+	blog_service.Do_work()
 }
